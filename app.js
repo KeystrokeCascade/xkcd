e8bcd79a5ab60ab1761c254437c4c73bf191eb7b
KeystrokeCascade <35793455+KeystrokeCascade@users.noreply.github.com>
2020-02-06 17:15:36 +1100

    Testing with image

--- app.js
+++ app.js
@@ -4,4 +4,13 @@ fetch('https://xkcd.now.sh/?comic=latest')
 	})
 	.then(data => {
 		console.log(JSON.stringify(data))
+		
+		var img = document.createElement("img");
+		img.src = data.img;
+		//img.width = width;
+		//img.height = height;
+		img.alt = data.alt;
+
+		// This next line will just add it to the <body> tag
+		document.body.appendChild(img);
 	})
